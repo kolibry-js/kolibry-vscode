8ed9a7d936d988a6d7c3e30f22f47a5e90668729
nyxb <contact@nyxb.email>
2023-11-30 21:00:59 +0100

    ✨ feat(view): add new view providers for VSCode extension
    - FoldingProvider for code folding functionality
    - PreviewProvider for live preview of content
    - SlideItem for individual slide representation in the tree view
    - SlidesProvider as a data provider for slides in the tree view
    
    These changes aim to enhance the user experience by providing better navigation and visualization tools within the VSCode extension environment.

---
+++ src/view/PreviewProvider.ts
@@ -0,0 +1,161 @@
+import type { WebviewView, WebviewViewProvider } from 'vscode'
+import { window } from 'vscode'
+import got from 'got'
+import { ctx } from '../ctx'
+import { config, setConfig } from '../config'
+import { getCurrentSlideIndex } from '../slides'
+import { isDarkTheme } from '../utils'
+
+export class PreviewProvider implements WebviewViewProvider {
+  public static readonly viewId = 'kolibry-preview'
+  public view: WebviewView | undefined
+
+  public updateColor() {
+    if (!this.view)
+      return
+
+    this.view.webview.postMessage({
+      target: 'kolibry',
+      type: 'css-vars',
+      vars: {
+        '--kolibry-slide-container-background': 'transparent',
+      },
+    })
+    this.view.webview.postMessage({
+      target: 'kolibry',
+      type: 'color-schema',
+      color: isDarkTheme() ? 'dark' : 'light',
+    })
+  }
+
+  public updateSlide(idx: number) {
+    if (!this.view)
+      return
+
+    this.view.webview.postMessage({ target: 'kolibry', type: 'navigate', no: idx + 1 })
+    this.updateColor()
+  }
+
+  public async refresh() {
+    const editor = window.activeTextEditor
+    if (!editor || editor.document !== ctx.doc)
+      return
+
+    if (!this.view)
+      return
+
+    const idx = getCurrentSlideIndex(editor)
+
+    this.view.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [ctx.ext.extensionUri],
+    }
+
+    this.view.webview.onDidReceiveMessage(async ({ command }) => {
+      if (command === 'config-port') {
+        const port = await window.showInputBox({
+          placeHolder: 'Server port',
+        })
+        if (port && !isNaN(+port)) {
+          await setConfig('port', +port || 3030)
+          this.refresh()
+        }
+      }
+    })
+    
+    let serverAddr = `http://127.0.0.1:${config.port}/`
+
+    const indexUrl = `${serverAddr}index.html`
+    const resolvedBody = await got.get(indexUrl, { responseType: 'text', resolveBodyOnly: true }).catch(() => null)
+    if (!resolvedBody) {
+      serverAddr = `http://[::1]:${config.port}/`
+    }
+    const url = `${serverAddr}${idx}?embedded=true`
+
+    this.view.webview.html = `
+<head>
+  <meta
+    http-equiv="Content-Security-Policy"
+    content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';"
+  />
+<head>
+<script>
+  const vscode = acquireVsCodeApi()
+  window.configPort = () => {
+    vscode.postMessage({
+      command: 'config-port'
+    })
+  }
+</script>
+<style>
+button {
+  background: var(--vscode-button-secondaryBackground);
+  color: var(--vscode-button-secondaryForeground);
+  border: none;
+  padding: 8px 12px;
+}
+button:hover {
+  background: var(--vscode-button-secondaryHoverBackground);
+}
+code {
+  font-size: 0.9em;
+  font-family: var(--vscode-editor-font-family);
+  background: var(--vscode-textBlockQuote-border);
+  border-radius: 4px;
+  padding: 3px 5px;
+}
+</style>
+<body>
+  <div style="text-align: center"><p>Kolibry server is not found on <code>${serverAddr}</code></p><p>please run <code style="color: #679bbb">$ kolibry</code> first</p><br><button onclick="configPort()">Config Server Port</button></div>
+</body>
+`
+
+    try {
+      await got.get(`${serverAddr}index.html`, { responseType: 'text', resolveBodyOnly: true })
+    }
+    catch {
+      return
+    }
+
+    this.view.webview.html = `
+<head>
+  <meta
+    http-equiv="Content-Security-Policy"
+    content="default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';"
+  />
+  <style>
+  body {
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+  iframe {
+    border: none;
+    width: 100%;
+    height: 100%;
+  }
+  </style>
+<head>
+<body>
+  <iframe id="iframe" src="${url}"></iframe>
+  <script>
+    var iframe = document.getElementById('iframe')
+    window.addEventListener('message', ({ data }) => {
+      if (data && data.target === 'kolibry') {
+        iframe.contentWindow.postMessage(data, '${serverAddr}')
+      }
+    })
+  </script>
+</body>
+`
+
+    setTimeout(() => this.updateColor(), 10)
+    setTimeout(() => this.updateColor(), 300)
+    setTimeout(() => this.updateColor(), 1000)
+  }
+
+  public async resolveWebviewView(webviewView: WebviewView) {
+    this.view = webviewView
+    this.refresh()
+  }
+}
